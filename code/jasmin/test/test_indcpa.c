ea37b98022a0802f1c7d7a62abf9019f57491c49
Peter Schwabe <peter@cryptojedi.org>
2020-02-26 15:58:59 +0100

    encryption implemented (not yet tested) in jasmin; pushing to sync

--- code/jasmin/test/test_indcpa.c
+++ code/jasmin/test/test_indcpa.c
@@ -10,21 +10,36 @@ int main(void)
   unsigned char sk1[KYBER_INDCPA_SECRETKEYBYTES];
   unsigned char pk0[KYBER_INDCPA_PUBLICKEYBYTES];
   unsigned char pk1[KYBER_INDCPA_PUBLICKEYBYTES];
+  unsigned char ct0[KYBER_INDCPA_BYTES];
+  unsigned char ct1[KYBER_INDCPA_BYTES];
 
-  unsigned char randomness[KYBER_SYMBYTES];
+  unsigned char randomness0[KYBER_SYMBYTES];
+  unsigned char randomness1[KYBER_SYMBYTES];
+  unsigned char message[KYBER_SYMBYTES];
 
   FILE *urandom = fopen("/dev/urandom", "r");
-  fread(randomness, KYBER_SYMBYTES, 1, urandom);
+  fread(randomness0, KYBER_SYMBYTES, 1, urandom);
+  fread(randomness1, KYBER_SYMBYTES, 1, urandom);
+  fread(message, KYBER_SYMBYTES, 1, urandom);
   fclose(urandom);
 
-  indcpa_keypair_jazz(pk1, sk1, randomness, zetas);
-  indcpa_keypair(pk0, sk0, randomness);
+  /* TEST KEYPAIR */
+  indcpa_keypair_jazz(pk1, sk1, randomness0, zetas);
+  indcpa_keypair(pk0, sk0, randomness0);
 
   for(int i=0;i<KYBER_INDCPA_SECRETKEYBYTES;i++)
     if(sk0[i] != sk1[i]) printf("error indcpa_keypair sk: %d\n", i);
 
   for(int i=0;i<KYBER_INDCPA_PUBLICKEYBYTES;i++)
     if(pk0[i] != pk1[i]) printf("error indcpa_keypair pk: %d\n", i);
+
+
+  /* TEST ENCRYPTION */
+  indcpa_enc(ct0, message, pk0, randomness1);
+  indcpa_enc_jazz(ct1, message, pk1, randomness1, zetas, zetas_inv);
+
+  for(int i=0;i<KYBER_INDCPA_BYTES;i++)
+    if(sk0[i] != sk1[i]) printf("error indcpa_enc: %d\n", i);
 
   return 0;
 }
