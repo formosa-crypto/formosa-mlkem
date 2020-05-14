67727b97bdc3e7c52a98c9448451d7c13689caff
Peter Schwabe <peter@cryptojedi.org>
2020-05-14 17:58:28 +0200

    Prepared gen_matrix for vectorization

--- code/jasmin/avx2/test/test_gen_matrix.c
+++ code/jasmin/avx2/test/test_gen_matrix.c
@@ -3,6 +3,7 @@
 
 #include "../params.h"
 
+extern void gen_matrix_old_jazz(int16_t *r, unsigned char *seed);
 extern void gen_matrix_jazz(int16_t *r, unsigned char *seed);
 
 int main(void)
@@ -15,12 +16,12 @@ int main(void)
   FILE *urandom = fopen("/dev/urandom", "r");
   fread(seed, 1, 32, urandom);
 
-  gen_matrix_jazz(aa0, seed);
+  gen_matrix_old_jazz(aa0, seed);
   gen_matrix_jazz(aa1, seed);
 
   for(i=0;i<KYBER_N*KYBER_K*KYBER_K;i++)
     if(aa0[i] != aa1[i])
-      printf("error gen_matrix %d: %d %d", i, aa0[i], aa1[i]);
+      printf("error gen_matrix %d: %d %d\n", i, aa0[i], aa1[i]);
 
   fclose(urandom);
 
