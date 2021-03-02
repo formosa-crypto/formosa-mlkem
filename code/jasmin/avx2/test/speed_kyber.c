ae687fb042ca81d92902fd2d37f011dccd7eb1d8
MQuaresma <miguelquaresma97@gmail.com>
2021-03-02 11:57:07 +0100

    Patch benchmark function to count for overhead

--- code/jasmin/avx2/test/speed_kyber.c
+++ code/jasmin/avx2/test/speed_kyber.c
@@ -40,12 +40,31 @@ static uint64_t average(uint64_t *t, size_t tlen) {
   return acc/tlen;
 }
 
+uint64_t cpucycles_overhead(void) {
+  uint64_t t0, t1, overhead = -1LL;
+  unsigned int i;
+
+  for(i=0;i<100000;i++) {
+    t0 = cpucycles();
+    __asm__ volatile ("");
+    t1 = cpucycles();
+    if(t1 - t0 < overhead)
+      overhead = t1 - t0;
+  }
+
+  return overhead;
+}
+
 void print_results(const char *s, uint64_t *t, size_t tlen) {
   size_t i;
+  static uint64_t overhead = -1;
+
+  if(overhead == (uint64_t)-1)
+    overhead = cpucycles_overhead();
 
   tlen--;
   for(i=0;i<tlen;++i)
-    t[i] = t[i+1] - t[i];
+    t[i] = t[i+1] - t[i] - overhead;
 
   printf("%s\n", s);
   printf("median: %llu cycles/ticks\n", (unsigned long long)median(t, tlen));
