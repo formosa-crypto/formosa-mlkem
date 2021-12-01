72bf5be8c1841fc361953171409f6f4c4daeb130
MQuaresma <miguelquaresma97@gmail.com>
2021-12-01 13:19:56 +0000

    Include KEM in speed benchmark

--- code/jasmin/avx2/speed.h
+++ code/jasmin/avx2/speed.h
@@ -46,4 +46,17 @@ void indcpa_enc_jazz(unsigned char *c,
 void indcpa_dec_jazz(unsigned char *m,
                      const unsigned char *c,
                      const unsigned char *sk);
+
+/* KEM functions */
+void crypto_kem_keypair_jazz(unsigned char *pk,
+                             unsigned char *sk,
+                             const unsigned char *randomness);
+
+void crypto_kem_enc_jazz(unsigned char *c,
+                         const unsigned char *m,
+                         const unsigned char *pk,
+                         const unsigned char *coins);
+void crypto_kem_dec_jazz(unsigned char *m,
+                         const unsigned char *c,
+                         const unsigned char *sk);
 #endif
