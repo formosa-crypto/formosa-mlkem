fe93f6824a865137f4de5355a15ba5139912f3a3
Tiago Oliveira <tfaoliveira@gmail.com>
2023-01-06 16:09:15 +0000

    bench: add avx2r; add tests (checksum based) -- jasmin implementations do not match expected checksums; pqclean implementations do; identifying the issue is still wip.

---
+++ bench/test/notrandombytes1.h
@@ -0,0 +1,8 @@
+#ifndef NOTRANDOMBYTES1_H
+#define NOTRANDOMBYTES1_H
+#include <stdint.h>
+
+uint8_t* __jasmin_syscall_randombytes__(uint8_t* x, uint64_t xlen);
+void randombytes(uint8_t* x, uint64_t xlen);
+
+#endif
