75ac8b8029db29462bbb859877b7b0734fac1d72
José Bacelar Almeida <jba@di.uminho.pt>
2024-10-18 18:26:33 +0100

    transition to new keccak

--- code/jasmin/mlkem_avx2/kem.h
+++ code/jasmin/mlkem_avx2/kem.h
@@ -16,24 +16,24 @@ void crypto_kem_dec(unsigned char *m,
                 const unsigned char *c,
                 const unsigned char *sk);
 
-void jade_kem_mlkem_mlkem768_amd64_avx2v_keypair_derand(unsigned char *pk,
+void jade_kem_mlkem_mlkem768_amd64_avx2_keypair_derand(unsigned char *pk,
                          unsigned char *sk,
                          const unsigned char *randomness);
 
-void jade_kem_mlkem_mlkem768_amd64_avx2v_enc_derand(unsigned char *c,
+void jade_kem_mlkem_mlkem768_amd64_avx2_enc_derand(unsigned char *c,
                      const unsigned char *m,
                      const unsigned char *pk,
                      const unsigned char *coins);
 
 
-void jade_kem_mlkem_mlkem768_amd64_avx2v_keypair(unsigned char *pk,
+void jade_kem_mlkem_mlkem768_amd64_avx2_keypair(unsigned char *pk,
                          unsigned char *sk);
 
-void jade_kem_mlkem_mlkem768_amd64_avx2v_enc(unsigned char *c,
+void jade_kem_mlkem_mlkem768_amd64_avx2_enc(unsigned char *c,
                      const unsigned char *m,
                      const unsigned char *pk);
 
-void jade_kem_mlkem_mlkem768_amd64_avx2v_dec(unsigned char *m,
+void jade_kem_mlkem_mlkem768_amd64_avx2_dec(unsigned char *m,
                      const unsigned char *c,
                      const unsigned char *sk);
 
