8ba1c5c7f11432188faa9e5c1ad6568200dd0b56
Peter Schwabe <peter@cryptojedi.org>
2025-04-28 13:02:36 +0200

    Some more progress on libjade extraction

--- extract-libjade.sh
+++ extract-libjade.sh
@@ -42,30 +42,48 @@ list_implementations() {
   echo "crypto_kem/mlkem1024/amd64/avx2"
 }
 
+flatten_jazz() {
+  cd $BUILDTMPDIR
+  pwd
+}
+
 gen_implementation() {
   if [ "${#}" -eq 2 ]; then
 
-    # start by realpath them (useful to run make)
-    implementation="${project_dir}"/src/"${1}"
-    directory="${2}"
+    IMPLEMENTATION="${1}"
+    TARGETDIR="${2}"
 
     # test if libjade DIRECTORY exists
-    if [ ! -d "${directory}" ]; then
-      fatal "target directory does not exist: ${directory} "
+    if [ ! -d "${TARGETDIR}" ]; then
+      fatal "target directory does not exist: ${TARGETDIR} "
     fi
 
-    echo ${implementation}
-    echo ${directory}
-
-    if [ -d "${project_dir}/build-tmp" ]; then
-      rm -r ${project_dir}/build-tmp
+    if [ -d ${BUILDTMPDIR} ]; then
+      rm -r ${BUILDTMPDIR}
     fi
-    mkdir ${project_dir}/build-tmp
+    mkdir ${BUILDTMPDIR}
 
-    
+    if [ ${IMPLEMENTATION} = "crypto_kem/mlkem768/amd64/ref" ];then
+      cp -r ${PROJECTDIR}/code/jasmin/768/ref/* ${BUILDTMPDIR}
+      flatten_jazz jkem.jazz ${TARGETDIR}/kyber768.jazz
+      cp "${BUILDTMPDIR}/api.h" "${TARGETDIR}/kyber768.h"
+    elif [ ${IMPLEMENTATION} = "crypto_kem/mlkem768/amd64/avx2" ];then
+      cp -r "${PROJECTDIR}/code/jasmin/768/avx2/*" ${BUILDTMPDIR}
+      flatten_jazz jkem.jazz ${TARGETDIR}/kyber768.jazz
+      cp "${BUILDTMPDIR}/api.h" "${TARGETDIR}/kyber768.h"
+    elif [ ${IMPLEMENTATION} = "crypto_kem/mlkem1024/amd64/ref" ];then
+      cp -r "${PROJECTDIR}/code/jasmin/1024/ref/*" ${BUILDTMPDIR}
+      flatten_jazz jkem.jazz ${TARGETDIR}/kyber1024.jazz
+      cp "${BUILDTMPDIR}/api.h" "${TARGETDIR}/kyber1024.h"
+    elif [ ${IMPLEMENTATION} = "crypto_kem/mlkem1024/amd64/avx2" ];then
+      cp -r "${PROJECTDIR}/code/jasmin/1024/avx2/*" ${BUILDTMPDIR}
+      flatten_jazz jkem.jazz ${TARGETDIR}/kyber1024.jazz
+      cp "${BUILDTMPDIR}/api.h" "${TARGETDIR}/kyber1024.h"
+    else
+      fatal "Implementation not supported: ${IMPLEMENTATION} "
+    fi
 
     return 0
-
 
   else
     fatal_usage "$0 --gen-implementation: number of required arguments 3 : provided $#"
@@ -74,8 +92,10 @@ gen_implementation() {
 
 main() {
   cd "$(dirname "${0}")"
-  project_dir="$(pwd -P)"
-  cd "${project_dir}"
+  PROJECTDIR="$(pwd -P)"
+  BUILDTMPDIR="${PROJECTDIR}/build-tmp"
+  JPP="${PROJECTDIR}/scripts/jpp"
+  cd "${PROJECTDIR}"
 
   local cmd; cmd="$1"; shift || fatal_usage "Please specify a command"
   case "${cmd}" in
