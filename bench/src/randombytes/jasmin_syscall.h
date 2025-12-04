2a0b6a4410c96af472d858dc0a7d1e246f1ea59c
gsd <gsd@cloud113.cluster.lsd.di.uminho.pt>
2025-12-04 14:00:03 +0000

    remove old bench dir

--- bench/src/randombytes/jasmin_syscall.h
+++
@@ -1,9 +0,0 @@
-#ifndef JASMIN_SYSCALL_H
-#define JASMIN_SYSCALL_H
-
-#include <stdint.h>
-
-uint8_t* __jasmin_syscall_randombytes__(uint8_t* x, uint64_t xlen)
-  __asm("__jasmin_syscall_randombytes__");
-
-#endif
--- bench/src/randombytes/randombytes1.h
+++
@@ -1,8 +0,0 @@
-#ifndef RANDOMBYTES_H
-#define RANDOMBYTES_H
-
-#include <stdint.h>
-
-void randombytes(uint8_t* x, uint64_t xlen);
-
-#endif
