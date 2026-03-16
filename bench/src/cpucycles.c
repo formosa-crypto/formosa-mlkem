892dfb9a469483837f6ac62ef4d2d46537b02847
Tiago Oliveira <tfaoliveira@gmail.com>
2026-03-16 23:31:16 +0000

    fxb: fixing some stuff

--- bench/src/cpucycles.c
+++ bench/src/cpucycles.c
@@ -5,14 +5,60 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-static inline uint64_t cpucycles(void)
+// try1
+#define cpucycles_begin cpuid_rdtsc
+#define cpucycles_end rdtscp_lfence
+
+//try2
+#if 0
+#define cpucycles_begin lfence_rdtsc
+#endif
+
+static inline uint64_t cpuid_rdtsc(void)
 {
-  uint64_t result;
+    uint32_t lo, hi;
 
-  __asm__ volatile ("rdtsc; shlq $32,%%rdx; orq %%rdx,%%rax"
-    : "=a" (result) : : "%rdx");
+    __asm__ volatile (
+        "cpuid\n\t"
+        "rdtsc"
+        : "=a"(lo), "=d"(hi)
+        : "a"(0)
+        : "rbx", "rcx", "memory"
+    );
 
-  return result;
+    return ((uint64_t)hi << 32) | lo;
+}
+
+static inline uint64_t lfence_rdtsc(void)
+{
+  uint32_t lo, hi;
+
+  __asm__ volatile (
+    "lfence\n\t"
+    "rdtsc"
+    : "=a"(lo), "=d"(hi)
+    :
+    : "memory"
+  );
+
+  return ((uint64_t)hi << 32) | lo;
+}
+
+//
+
+static inline uint64_t rdtscp_lfence(void)
+{
+  uint32_t lo, hi;
+
+  __asm__ volatile (
+    "rdtscp\n\t"
+    "lfence"
+    : "=a"(lo), "=d"(hi)
+    :
+    : "rcx", "memory"
+  );
+
+  return ((uint64_t)hi << 32) | lo;
 }
 
 static int cmp_uint64(const void *a, const void *b)
@@ -22,21 +68,12 @@ static int cmp_uint64(const void *a, const void *b)
   return 0;
 }
 
-static uint64_t median(uint64_t *l, size_t llen)
+static uint64_t cpucycles_median(uint64_t *l, size_t llen)
 {
   qsort(l,llen,sizeof(uint64_t),cmp_uint64);
 
   if(llen%2) return l[llen/2];
   else return (l[llen/2-1]+l[llen/2])/2;
-}
-
-static uint64_t cpucycles_median(uint64_t *cycles, size_t timings)
-{
-  size_t i;
-  for (i = 0; i < timings-1; i++)
-  { cycles[i] = cycles[i+1] - cycles[i]; }
-
-  return median(cycles, timings-1);
 }
 
 #endif
